7af5f91d11b11acec2bc1ef1b197bf15defd100f
Jon Perkins <captainperk81@gmail.com>
2018-07-23 20:32:29 -0400

    Implementing IPageElement

---
+++ AutomatedUITestingFrameworkDemo/Adapters/SeleniumElement.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomatedUITestingFrameworkDemo.Adapters
+{
+    public class SeleniumElement
+    {
+        public SeleniumElement(IWebElement element)
+        {
+            Element = element;
+        }
+
+        internal IWebElement Element { get; }
+
+        public void Click()
+        {
+            Element.Click();
+        }
+
+        public bool Displayed()
+        {
+            return Element.Displayed;
+        }
+
+        public string GetAttributeValue(string attributeName)
+        {
+            return Element.GetAttribute(attributeName);
+        }
+    }
+}
