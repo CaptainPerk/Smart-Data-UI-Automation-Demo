edc1f54dc163e212b665ac6f53998473f725bfcb
Jon Perkins <captainperk81@gmail.com>
2018-07-23 21:28:53 -0400

    Adding Pages and Navigation to SmartDataWebsite

--- SmartDataFramework/SmartDataWebsite.cs
+++ SmartDataFramework/SmartDataWebsite.cs
@@ -1,6 +1,22 @@
-﻿namespace SmartDataFramework
+﻿using AutomatedUITestingFrameworkDemo.Interfaces;
+using SmartDataFramework.Pages;
+
+namespace SmartDataFramework
 {
     public class SmartDataWebsite
     {
+        public AboutUsPage AboutUsPage = new AboutUsPage();
+        public CareersPage CareersPage = new CareersPage();
+        public CaseStudiesPage CaseStudiesPage = new CaseStudiesPage();
+        public ContactPage ContactPage = new ContactPage();
+        public HomePage HomePage = new HomePage();
+        public IncubatorPage IncubatorPage = new IncubatorPage();
+        public LunchAndLearnPage LunchAndLearnPage = new LunchAndLearnPage();
+        public ServicesPage ServicesPage = new ServicesPage();
+
+        public void UseNavigationBarToNavigateTo(IBrowserAutomator automator, IPage page)
+        {
+            automator.GetElementByCssSelector(page.NavBarCssSelector).Click();
+        }
     }
 }
